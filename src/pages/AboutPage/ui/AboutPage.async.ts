bd7d853f563e800e07aaf438695194d534773a80
Vitali <vit-sema@yandex.ru>
2023-10-27 18:23:47 +0300

    fix page imports

--- src/pages/AboutPage/ui/AboutPage.async.ts
+++ src/pages/AboutPage/ui/AboutPage.async.ts
@@ -5,6 +5,6 @@ export const AboutPageAsync = lazy(
 		new Promise((resolve) => {
 			// @ts-ignore
 			// ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-			setTimeout(() => resolve(import('./AboutPage')), 1500);
+			setTimeout(() => resolve(import('../ui/AboutPage')), 1500);
 		})
 );
