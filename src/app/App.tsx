9617ddc3096c1afb0a591028823e26d7c1b717c8
Vitali <vit-sema@yandex.ru>
2023-11-16 11:50:15 +0300

    add sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 import './styles/index.scss';
 
 export const App = () => {
@@ -10,7 +11,10 @@ export const App = () => {
 	return (
 		<div className={classNames('app', {}, [theme])}>
 			<Navbar />
-			<AppRouter />
+			<div className="content-page">
+				<Sidebar />
+				<AppRouter />
+			</div>
 		</div>
 	);
 };
